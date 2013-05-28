f8b6d672fe5192b5c30c27a9d02f08bb8f1f417c
Tafhim <tafhim@sphinxcorporation.com>
2013-05-28 10:00:17 +0600

    Implemented (copied) the initial framework

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$configs = array();
+$configs['db_host_sn'] = 'localhost';
+$configs['db_user_sn'] = 'root';
+$configs['db_pass_sn'] = 'tafhim';
+$configs['db_name_sn'] = 'project';
+
+
+?>
